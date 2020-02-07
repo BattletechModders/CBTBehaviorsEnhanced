d14ae4e2ad6fd74dec82c6c93c8abba434b83886
Bradley Burcar <btburcar@gmail.com>
2020-02-06 23:33:50 -0500

    Bah.

--- CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Patches/CombatSelectionHandlerPatches.cs
+++ CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Patches/CombatSelectionHandlerPatches.cs
@@ -5,17 +5,17 @@ namespace CBTBehaviorsEnhanced.Patches {
 
     [HarmonyPatch(typeof(SelectionState), "CanDeselect", MethodType.Getter)]
     public static class SelectionState_CanDeselect_Getter {
-        public static bool Prefix(SelectionState __instance, ref bool __result) {
+        //public static bool Prefix(SelectionState __instance, ref bool __result) {
 
-            if (ModState.Combat.TurnDirector.IsInterleaved) {
-                __result = !__instance.SelectedActor.HasBegunActivation && !__instance.SelectedActor.StoodUpThisRound;
-            } else {
-                // If we already have an order going, don't allow the player to select another unit
-                __result = ModState.Combat.StackManager.IsAnyOrderActiveIncludeNonInterleaved && 
-                    (!__instance.SelectedActor.HasBegunActivation || (__instance.Orders != null && __instance.ConsumesFiring));
-            }
+        //    if (ModState.Combat.TurnDirector.IsInterleaved) {
+        //        __result = !__instance.SelectedActor.HasBegunActivation && !__instance.SelectedActor.StoodUpThisRound;
+        //    } else {
+        //        // If we already have an order going, don't allow the player to select another unit
+        //        __result = ModState.Combat.StackManager.IsAnyOrderActiveIncludeNonInterleaved && 
+        //            (!__instance.SelectedActor.HasBegunActivation || (__instance.Orders != null && __instance.ConsumesFiring));
+        //    }
 
-            return false;
-        }
+        //    return false;
+        //}
     }
 }
