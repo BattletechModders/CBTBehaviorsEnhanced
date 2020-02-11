ca0b000fd537c22df85ecad34b8dac447c7465a9
Gerrit Kitts <gkitts@gmail.com>
2020-02-11 14:41:53 -0500

    - Fix autoselection in non-interleaved mode

--- CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Patches/CombatSelectionHandlerPatches.cs
+++ CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Patches/CombatSelectionHandlerPatches.cs
@@ -1,21 +1,58 @@
-﻿using BattleTech.UI;
+﻿using BattleTech;
+using BattleTech.UI;
 using Harmony;
+using us.frostraptor.modUtils;
 
 namespace CBTBehaviorsEnhanced.Patches {
 
-    //[HarmonyPatch(typeof(SelectionState), "CanDeselect", MethodType.Getter)]
-    //public static class SelectionState_CanDeselect_Getter {
-    //    //public static bool Prefix(SelectionState __instance, ref bool __result) {
+    //[HarmonyPatch(typeof(CombatSelectionHandler), "AutoSelectActor")]
+    //public static class CombatSelectionHandler_AutoSelectActor {
+    //    public static void Prefix(CombatSelectionHandler __instance) {
+    //        Traverse combatT = Traverse.Create(__instance).Property("Combat");
+    //        CombatGameState combat = combatT.GetValue<CombatGameState>();
+    //        Mod.Log.Debug($"AUTO SELECT DETAILS:  hasAvailableUnits: {__instance.HasAvailableUnits}" +
+    //            $"  isInterleaved: {combat.TurnDirector.IsInterleaved}  " +
+    //            $"  autoSelectInterleaved: {combat.Constants.CombatUIConstants.AutoSelectDuringInterleaved}" +
+    //            $"  autoSelectNonInterleaved: {combat.Constants.CombatUIConstants.AutoSelectDuringNonInterleaved}" +
+    //            $"  selectedActor: '{CombatantUtils.Label(__instance.SelectedActor)}'" +
+    //            $"  localTeamActive: {combat.LocalPlayerTeam.IsActive}");
 
-    //    //    if (ModState.Combat.TurnDirector.IsInterleaved) {
-    //    //        __result = !__instance.SelectedActor.HasBegunActivation && !__instance.SelectedActor.StoodUpThisRound;
-    //    //    } else {
-    //    //        // If we already have an order going, don't allow the player to select another unit
-    //    //        __result = ModState.Combat.StackManager.IsAnyOrderActiveIncludeNonInterleaved && 
-    //    //            (!__instance.SelectedActor.HasBegunActivation || (__instance.Orders != null && __instance.ConsumesFiring));
-    //    //    }
-
-    //    //    return false;
-    //    //}
+    //        if (combat.LocalPlayerTeam.IsActive) {
+    //            AbstractActor nextAvailableUnit = combat.LocalPlayerTeam.GetNextAvailableUnit(__instance.SelectedActor);
+    //            Mod.Log.Debug($" == LOCAL PLAYER ACTIVE, NEXT AVAILABLE: {CombatantUtils.Label(nextAvailableUnit)}");
+    //        }
+    //    }
     //}
+
+    [HarmonyPatch(typeof(CombatSelectionHandler), "RemoveCompletedItems")]
+    public static class CombatSelectionHandler_RemoveCompletedItems {
+        public static void Prefix(CombatSelectionHandler __instance, ref bool __state) {
+            __state = false;
+
+            if (__instance == null || __instance.ActiveState == null) { return; }
+            if (__instance.SelectedActor != null && __instance.SelectedActor.IsDead) { return; }
+
+            Mod.Log.Debug($"CSH:RCI");
+
+            AbstractActor selectedActor = __instance.SelectedActor;
+            Traverse combatT = Traverse.Create(__instance).Property("Combat");
+            CombatGameState combat = combatT.GetValue<CombatGameState>();
+
+            Mod.Log.Debug($" == RCI - selectedActor: {CombatantUtils.Label(selectedActor)}" +
+                $"  isInterleaved: {combat.TurnDirector.IsInterleaved}  isComplete: {__instance.ActiveState.IsComplete}");
+
+            if (__instance.ActiveState.IsComplete && !combat.TurnDirector.IsInterleaved) {
+                __state = true;
+            }
+        }
+
+        public static void Postfix(CombatSelectionHandler __instance, ref bool __state) {
+            if (__state) {
+                // Because we override movement states to ConsumesFiring=true during non-interleaved mode, we have to force an auto-select
+                Mod.Log.Debug("INVOKING AUTO SELECT!");
+                __instance.AutoSelectActor();
+            }
+        }
+    }
+
 }
--- CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Patches/MovementPatches.cs
+++ CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Patches/MovementPatches.cs
@@ -134,37 +134,6 @@ namespace CBTBehaviorsEnhanced {
             }
         }
 
-        [HarmonyPatch(typeof(Team), "DoneWithAllAvailableActors")]
-        public static class Team_DoneWithAllAvailableActors {
-            private static void Prefix(Team __instance, List<IStackSequence> __result) {
-                Mod.Log.Info($"T:DWAAA invoked");
-                if (!__instance.IsLocalPlayer) { return; }
-
-                if (__instance.Combat.TurnDirector.IsInterleavePending) {
-                    if (__result == null) {
-                        Mod.Log.Info("Result was null, adding a new list.");
-                        __result = new List<IStackSequence>();
-                    }
-
-                    int numUnitsEndingActivation = 0;
-                    foreach (AbstractActor unit in __instance.units) {
-                        Mod.Log.Info($"Processing unit: {unit.DisplayName}_{unit.GetPilot().Name}");
-                        if (!unit.IsCompletingActivation && !unit.IsDead && !unit.IsFlaggedForDeath) {
-                            Mod.Log.Info($"  Ending activation ");
-                            IStackSequence item = unit.DoneWithActor();
-                            numUnitsEndingActivation++;
-                            __result.Add(item);
-                        }
-                    }
-
-                    Traverse numUnitsEndingActivationT = Traverse.Create(__instance).Field("numUnitsEndingActivation");
-                    int currentValue = numUnitsEndingActivationT.GetValue<int>();
-                    numUnitsEndingActivationT.SetValue(currentValue + numUnitsEndingActivation);
-                }
-
-            }
-        }
-
         // Prevents losing evasion when attacked
         [HarmonyPatch(typeof(AbstractActor), "ResolveAttackSequence", null)]
         public static class AbstractActor_ResolveAttackSequence_Patch {
