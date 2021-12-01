a10aed11fa4ca76f1cd57f626c3238961c7ea9c0
IceRaptor <gkitts@gmail.com>
2021-12-01 12:45:20 -0500

    - Prevent V key from selecting melee targets that are invalid
    (cherry picked from commit 107f4694556ee240ae0b313a77af8b22bd206ad1)

--- CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Melee/SelectionStatePatches.cs
+++ CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Melee/SelectionStatePatches.cs
@@ -3,6 +3,7 @@ using BattleTech.UI;
 using CBTBehaviorsEnhanced.MeleeStates;
 using Harmony;
 using IRBTModUtils;
+using IRBTModUtils.Extension;
 using System.Collections.Generic;
 using us.frostraptor.modUtils;
 
@@ -53,6 +54,53 @@ namespace CBTBehaviorsEnhanced.Melee
                     $"current: {selectedMech.CurrentStability}  projectedNew: {selectedAttack.AttackerInstability}  " +
                     $"totalChange: {newStability}  afterDump: {minStability}");
                 __result = minStability;
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(SelectionStateMove), "GetAllMeleeTargets")]
+    static class SelectionStateMove_GetAllMeleeTargets
+    {
+        static void Postfix(SelectionStateMove __instance, ref List<ICombatant> __result)
+        {
+            if (__instance == null || __result == null) return;
+            if (__instance.SelectedActor == null || !(__instance.SelectedActor is Mech)) return;
+
+
+            // If there are valid results, check for any melee state that would prevent attacks
+            if (__result != null && __result.Count > 0)
+            {
+                Mech selectedMech = __instance.SelectedActor as Mech;
+                Mod.MeleeLog.Debug?.Write($" filtering targets of mech: {selectedMech.DistinctId()} for melee state");
+                List<ICombatant> filteredList = new List<ICombatant>();
+                foreach (ICombatant target in __result)
+                {
+                    if (target is AbstractActor targetActor)
+                    {
+                        Mod.MeleeLog.Debug?.Write($"Finding selection melee state for actor: {__instance.SelectedActor.DistinctId()} " +
+                            $"at pos: {__instance.PreviewPos} vs. target: {targetActor.DistinctId()}");
+                        MeleeState meleeState = new MeleeState(selectedMech, __instance.PreviewPos, targetActor);
+
+                        bool hasValidAttack = meleeState.Charge.IsValid || meleeState.Kick.IsValid ||
+                            meleeState.PhysicalWeapon.IsValid || meleeState.Punch.IsValid;
+                        if (!hasValidAttack)
+                        {
+                            Mod.MeleeLog.Debug?.Write($" -- No valid attack detected, removing melee candidate");
+                        }
+                        else
+                        {
+                            Mod.MeleeLog.Debug?.Write($"  -- Valid attaks, keeping candidate");
+                            filteredList.Add(targetActor);
+                        }
+                    }
+                    else
+                    {
+                        Mod.MeleeLog.Debug?.Write($" -- skipping non-actor target: {target.DistinctId()}");
+                    }
+                }
+
+                __result.Clear();
+                __result.AddRange(filteredList);
             }
         }
     }
