bb34161dfd8480e4bbe853118fbe5ee8c13e517b
IceRaptor <gkitts@gmail.com>
2021-12-09 21:45:18 -0500

    - Fix issue where multipliers for TT-adherence (1.5) and vanilla speed were stacking incorrectly - Fix issue where all mechs were being treated as legged

--- CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Move/CBTBEMechMoveModifier.cs
+++ CBTBehaviorsEnhanced/CBTBehaviorsEnhanced/Move/CBTBEMechMoveModifier.cs
@@ -8,32 +8,71 @@ using System.Collections.Generic;
 namespace CBTBehaviorsEnhanced.Move
 {
 
-    static class Heat_MoveModifier
+    internal abstract class IRBTModUtilMoveModifier
     {
-        // Should be first if at all possible
-        public const int Priority = 0;
-        public const string Name = "CBTBE_Heat";
-        public static float WalkMod(Mech mech, float current)
+        public abstract int Priority { get; }
+        public abstract string Name { get; }
+
+        public abstract float WalkMod(Mech mech, float current);
+        public abstract float RunMod(Mech mech, float current);
+    }
+
+    // Should be first if at all possible
+    internal class TTRun_MoveModifier : IRBTModUtilMoveModifier
+    {
+        public override int Priority { get { return 0; } }
+        public override string Name { get { return "CBTBE_TTRun"; } }
+
+        public override float WalkMod(Mech mech, float current)
+        {
+            return current;
+        }
+
+        public override float RunMod(Mech mech, float current)
+        {
+            if (mech.IsVehicle() || mech.IsNaval() || mech.IsTrooper()) return current;
+
+            float runMultiMod = mech.StatCollection.GetValue<float>(ModStats.RunMultiMod);
+            float runMulti = Mod.Config.Move.RunMulti;
+            if (runMultiMod != 0f)
+            {
+                runMulti += runMultiMod;
+            }
+            Mod.MoveLog.Debug?.Write($"run mod => {runMulti} from CBTBE_RunMultiMod: {runMultiMod} + modConfig: {Mod.Config.Move.RunMulti}");
+
+            float resetSpeed = mech.WalkSpeed * runMulti;
+            Mod.MoveLog.Debug?.Write($"Resetting runSpeed to {resetSpeed} for actor: {mech.DistinctId()}");
+
+            return resetSpeed;
+        }
+    }
+
+    internal class Heat_MoveModifier : IRBTModUtilMoveModifier
+    {
+        public override int Priority { get { return 0; } }
+        public override string Name { get { return "CBTBE_Heat"; } }
+
+        public override float WalkMod(Mech mech, float current)
         {
             if (mech.IsVehicle() || mech.IsNaval() || mech.IsTrooper()) return current;
             if (mech.CurrentHeat == 0) return current;
 
             // Check for overheat penalties
-            int movePenaltyKey = 0;
+            int movePenalty = 0;
             foreach (KeyValuePair<int, int> kvp in Mod.Config.Heat.Movement)
             {
                 if (mech.CurrentHeat >= kvp.Key)
                 {
-                    movePenaltyKey = kvp.Value;
+                    movePenalty = kvp.Value;
                 }
             }
 
-            float movePenaltyDist = movePenaltyKey * Mod.Config.Move.HeatMovePenalty;
+            float movePenaltyDist = movePenalty * Mod.Config.Move.HeatMovePenalty;
             Mod.MoveLog.Debug?.Write($"Mech: {mech.DistinctId()} has walk heat movePenalty: {movePenaltyDist}");
             return current - movePenaltyDist;
         }
 
-        public static float RunMod(Mech mech, float current)
+        public override float RunMod(Mech mech, float current)
         {
             if (mech.IsVehicle() || mech.IsNaval() || mech.IsTrooper()) return current;
             if (mech.CurrentHeat == 0) return current;
@@ -54,45 +93,18 @@ namespace CBTBehaviorsEnhanced.Move
         }
     }
 
-    public static class RunMultiMod_MoveModifier
+    // Should be last if possible
+    internal class Legged_MoveModifier : IRBTModUtilMoveModifier
     {
-        // Should be first if at all possible
-        public const int Priority = 5;
-        public const string Name = "CBTBE_RunMultiMod";
-        public static float WalkMod(Mech mech, float current)
-        {
-            return current;
-        }
+        public override int Priority { get { return 100; } }
+        public override string Name { get { return "CBTBE_Legged"; } }
 
-        public static float RunMod(Mech mech, float current)
-        {
-            float runMultiMod = mech.StatCollection.GetValue<float>(ModStats.RunMultiMod);
-            float runMulti = Mod.Config.Move.RunMulti;
-            if (runMultiMod != 0f)
-            {
-                runMulti += runMultiMod;
-            }
-            Mod.MoveLog.Debug?.Write($"run mod => {runMulti} from : CBTBE_RunMultiMod: {runMultiMod} + modConfig: {Mod.Config.Move.RunMulti}");
-
-            float newRun = current * runMulti;
-            Mod.MoveLog.Debug?.Write($"newRun = {newRun} => currentRun: {current} x runMulti: {runMulti}");
-
-            return newRun;
-
-        }
-    }
-
-    public static class Legged_MoveModifier
-    {
-        // Should be first if at all possible
-        public const int Priority = 100;
-        public const string Name = "CBTBE_Legged";
-        public static float WalkMod(Mech mech, float current)
+        public override float WalkMod(Mech mech, float current)
         {
             if (mech.IsVehicle() || mech.IsNaval() || mech.IsTrooper()) return current;
 
             int destroyedLegs = DestroyedLegsCount(mech);
-            if (destroyedLegs == 0) return 0;
+            if (destroyedLegs == 0) return current;
 
             if (mech.IsQuadMech())
             {
@@ -117,12 +129,12 @@ namespace CBTBehaviorsEnhanced.Move
 
         }
 
-        public static float RunMod(Mech mech, float current)
+        public override float RunMod(Mech mech, float current)
         {
             if (mech.IsVehicle() || mech.IsNaval() || mech.IsTrooper()) return current;
 
             int destroyedLegs = DestroyedLegsCount(mech);
-            if (destroyedLegs == 0) return 0;
+            if (destroyedLegs == 0) return current;
 
             if (mech.IsQuadMech())
             {
