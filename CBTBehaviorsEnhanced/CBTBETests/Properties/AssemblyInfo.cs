f54aa7a89f05057c3b0c650e4f078635f8b15794
IceRaptor <gkitts@gmail.com>
2023-02-24 10:47:22 -0500

    - Update project to new visual studio format

--- CBTBehaviorsEnhanced/CBTBETests/Properties/AssemblyInfo.cs
+++ CBTBehaviorsEnhanced/CBTBETests/Properties/AssemblyInfo.cs
@@ -1,20 +1,7 @@
-using System.Reflection;
+﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
-
-[assembly: AssemblyTitle("CBTBETests")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CBTBETests")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
 
 [assembly: Guid("23d274d4-82ab-435b-8840-7b6eec42c08b")]
-
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
